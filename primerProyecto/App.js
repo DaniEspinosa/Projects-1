76e804c150023dbdbc1d1ab1b2088cc046725167
Daniel Espinosa Mauri <92428065+DaniEspinosa@users.noreply.github.com>
2022-11-07 14:22:08 +0100

    asd

--- primerProyecto/App.js
+++ primerProyecto/App.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Switch, StyleSheet, Image, ScrollView } from 'react-native'
-import img1 from './img/images.jpg'
+import img1 from './img/homer.gif'
 
 export default function App() {
   const [isEnabled, setIsEnabled] = useState(false);
