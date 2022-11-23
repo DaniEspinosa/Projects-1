74b992f0d15f177c83e74799455270ea478ef4e4
Daniel Espinosa Mauri <92428065+DaniEspinosa@users.noreply.github.com>
2022-11-23 11:49:19 +0100

    asd

---
+++ AccesoADatos/ProyectoNuevo/main.js
@@ -0,0 +1,10 @@
+document.write('adios mundo')
+
+const userName = {
+    name: 'Dani',
+    surname: 'Espinosa',
+    age: 19,
+    friends: ['Carlos', 'Andres', 'Lucas']
+}
+
+console.log(userName)
