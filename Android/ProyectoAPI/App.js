c7d51f9f28a82d2757e7db806d6106023404d6d5
Daniel Espinosa Mauri <92428065+DaniEspinosa@users.noreply.github.com>
2023-01-18 12:56:20 +0100

    asdsad

--- Android/ProyectoAPI/App.js
+++ Android/ProyectoAPI/App.js
@@ -5,7 +5,7 @@ export default function App() {
   const [fruits, setFruits] = useState(null);
 
   useEffect(() => {
-    fetch("http://10.0.2.2:8080/fruits")
+    fetch("http://10.88.9.74:8080/fruits")
       .then(response => response.json())
       .then((responseJson) => {
         console.log('getting data from fetch', responseJson);
