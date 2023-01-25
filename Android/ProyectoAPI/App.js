edc49da39b9574582ad15e198f0abf675aa1fd98
Daniel Espinosa Mauri <92428065+DaniEspinosa@users.noreply.github.com>
2023-01-25 14:47:59 +0100

    asd

--- Android/ProyectoAPI/App.js
+++ Android/ProyectoAPI/App.js
@@ -1,5 +1,10 @@
 import React, { useEffect, useState } from 'react';
-import { Text, FlatList, StyleSheet, RefreshControl, ScrollView } from 'react-native';
+import { StyleSheet, Button, RefreshControl, Text, View, FlatList, ScrollView, Image, TextInput, Alert } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import { IconName } from "react-icons/hi2";
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 const wait = (timeout) => {
   return new Promise(resolve => {
@@ -7,18 +12,21 @@ const wait = (timeout) => {
   });
 }
 
-export default function App() {
-  const [fruits, setFruits] = useState(null);
+const url = "http://10.88.1.224:8080/fruits"
+
+function HomeScreen() {
+  const [frutas, setFrutas] = useState(null);
   const [refreshing, setRefreshing] = useState(false);
   const [loading, setLoading] = useState(false);
+  const [imagen, setImagen] = useState(null);
 
   useEffect(() => {
     setLoading(true)
-    fetch("http://192.168.137.1a:8080/fruits")
+    fetch("http://10.88.1.224:8080/fruits")
       .then(response => response.json())
       .then((responseJson) => {
         console.log('getting data from fetch', responseJson);
-        setFruits(responseJson);
+        setFrutas(responseJson);
         setLoading(false)
       })
       .catch(error => console.log('errores', error));
@@ -30,27 +38,90 @@ export default function App() {
   }, []);
 
   const printElement = ({ item }) => {
-    
-      return (
-        <ScrollView refreshControl={<RefreshControl refreshing={refreshing} onRefresh={onRefresh} />}>
-          <Text style={styles.text}>{item.id}: {item.name}, {item.price}€</Text>
-        </ScrollView>
-      )
-    
+    return (
+      <ScrollView refreshControl={<RefreshControl refreshing={refreshing} onRefresh={onRefresh} />}>
+        <Text style={styles.text}>{item.id}: {item.name}, {item.price}€</Text>
+      </ScrollView>
+    )
   }
 
   return (
     <FlatList
-      data={fruits}
+      data={frutas}
       renderItem={printElement}
-      keyExtractor={fruits => fruits.id}
     />
+  );
+}
+
+function SettingsScreen() {
+  const [form, setForm] = useState({ id: 0, name: "", price: "" })
+  return (
+    <View style={styles.botones}>
+      <TextInput placeholder='Nombre de la fruta' onChangeText={value => setForm({ ...form, name: value })} value={form.name} style={styles.input} />
+      <TextInput placeholder='Precio de la fruta' onChangeText={value => setForm({ ...form, price: value })} value={form.price} style={styles.input} />
+      <Button
+        onPress={() => {
+          SubirFruta(form);
+        }}
+        title={"Nueva Fruta"}
+      />
+    </View>
   )
 }
 
+function SubirFruta(data) {
+  fetch(url, {
+    method: 'POST',
+    body: JSON.stringify(data),
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
+    }
+  })
+    .then(response => response.json())
+}
+
+const Tab = createBottomTabNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+            if (route.name === 'Frutas') {
+              iconName = focused ? 'home' : 'home-outline';
+            } else if (route.name === 'SubirFruta') {
+              iconName = focused ? 'nutrition' : 'nutrition-outline';
+            }
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+          tabBarActiveTintColor: 'orange',
+          tabBarInactiveTintColor: 'grey',
+        })}
+      >
+        <Tab.Screen name="Frutas" component={HomeScreen} />
+        <Tab.Screen name="Subir Frutas" component={SettingsScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
+
 const styles = StyleSheet.create({
+  botones: {
+    paddingTop: 20,
+    paddingBottom: 20,
+    paddingLeft: 30,
+    paddingRight: 30,
+    borderRadius: 200,
+  },
   text: {
-    fontSize: 20,
-    margin: 15
+    fontSize: 30,
+    margin: 10
+  },
+  input: {
+    borderWidth: 2,
+    margin: 10
   }
-});
+})
