51446469f649b2f522c6a9c04223a5c4d0e3db3c
Daniel Espinosa Mauri <92428065+DaniEspinosa@users.noreply.github.com>
2023-01-31 14:41:35 +0100

    asdsad

--- AccesoADatos/ProyectoNode2/src/controller/indexRoutex.js
+++ AccesoADatos/ProyectoNode2/src/controller/indexRoutex.js
@@ -14,3 +14,4 @@ export const postMetodo = async (req, res) => {
     const [result] = await pool.query('SELECT 1+1 as result')
     res.json(result[0])
 }
+
--- AccesoADatos/ProyectoNode2/src/routes/router.js
+++ AccesoADatos/ProyectoNode2/src/routes/router.js
@@ -1,6 +1,9 @@
 import { Router } from 'express';
 import { pool } from '../db.js';
+import { PrismaClient } from '@prisma/client'
 import { vistaHome, vistaLogin, vistaRegistro, postMetodo } from '../controller/indexRoutex.js';
+
+const prisma = new PrismaClient()
 const router = Router()
 
 // Crear nuestras rutas para las diferentes paginas
@@ -8,5 +11,17 @@ router.get('/', vistaHome)
 router.get('/login', vistaLogin)
 router.get('/registro', vistaRegistro)
 router.post('/', postMetodo)
+
+router.get('/daniel', async (req, res) => {
+    await prisma.Users.create({
+        data: {
+            name: "Daniel"
+        }
+    })
+    console.log(await prisma.Users.findMany())
+})
+
+
+
 
 export default router;
